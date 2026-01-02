d4a10092c6d1b743b655fd1dc677d50084e94c26
Matt Oskamp <mattoskamp@gmail.com>
2026-01-02 09:57:20 -0500

    day 3

--- src/day3.ts
+++ src/day3.ts
@@ -18,9 +18,39 @@ export function solveForMaxJoltagePart1(bank: string) {
   return currentMaxJoltage;
 }
 
+export function getMaxJoltageIndex(subBank: string) {
+  let maxDigit: number = 0;
+  let maxIndex: number = 0;
+  for (let i = 0; i < subBank.length; i++) {
+    const digit = parseInt(subBank[i] ?? "0");
+    if (digit > maxDigit) {
+      maxDigit = digit;
+      maxIndex = i;
+    }
+  }
+  return maxIndex;
+}
+
+export function solveForMaxJoltagePart2(bank: string, digitsRemaining: number) {
+  let bankCopy = bank;
+  let joltage = "";
+  console.log("bank: ", bank);
+  for (let i = digitsRemaining; i > 0; i--) {
+    let subBank = bankCopy.slice(0, bankCopy.length + 1 - i);
+    console.log("bankCopy: ", bankCopy);
+    // console.log("digitsRemaining: ", i);
+    // console.log("subBank: ", subBank);
+    let index = getMaxJoltageIndex(subBank);
+    // console.log("index: ", index);
+    joltage += bankCopy[index ?? "0"];
+    bankCopy = bankCopy.slice(index + 1);
+  }
+  return parseInt(joltage);
+}
+
 import input from "./inputs/Day 3 Advent of Code 2025.txt";
 
-const banks = input.split("\n");
+const banks = input.split("\n").filter((bank) => bank.length > 0);
 
-const maxJoltages = banks.map(solveForMaxJoltagePart1);
+const maxJoltages = banks.map((bank) => solveForMaxJoltagePart2(bank, 12));
 console.log(maxJoltages.reduce((acc, curr) => acc + curr, 0));
--- src/tests/day3.test.ts
+++ src/tests/day3.test.ts
@@ -1,9 +1,16 @@
 import { expect, test } from "bun:test";
-import { solveForMaxJoltagePart1 } from "../day3";
+import { solveForMaxJoltagePart1, solveForMaxJoltagePart2 } from "../day3";
 
 test("should solve for max voltage", () => {
   expect(solveForMaxJoltagePart1(`987654321111111`)).toBe(98);
   expect(solveForMaxJoltagePart1(`811111111111119`)).toBe(89);
   expect(solveForMaxJoltagePart1(`234234234234278`)).toBe(78);
   expect(solveForMaxJoltagePart1(`818181911112111`)).toBe(92);
+});
+
+test("should solve for max joltage part 2", () => {
+  expect(solveForMaxJoltagePart2(`987654321111111`, 12)).toBe(987654321111);
+  expect(solveForMaxJoltagePart2(`811111111111119`, 12)).toBe(811111111119);
+  expect(solveForMaxJoltagePart2(`234234234234278`, 12)).toBe(434234234278);
+  expect(solveForMaxJoltagePart2(`818181911112111`, 12)).toBe(888911112111);
 });
